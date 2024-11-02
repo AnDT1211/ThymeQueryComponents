6c53bd7203fe932b080bfd77515a7d35de5e5bd7
AnDT1211 <anthaido.bk@gmail.com>
2024-11-02 06:21:07 -0700

    init

---
+++ src/main/java/com/andt/thymequerycomponents/ThymeQueryComponentsApplication.java
@@ -0,0 +1,13 @@
+package com.andt.thymequerycomponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeQueryComponentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeQueryComponentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andt/thymequerycomponents/ThymeQueryComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andt.thymequerycomponents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeQueryComponentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
